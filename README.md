30d32e287fd880a1f62f457f12119925ca329b94
budickda <daniel@budick.eu>
2016-01-10 14:28:39 +0100

    updated readme

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Scrapping is deactivating. Use it at your own discression.
 This is just a proof of concept.
 For more information contact me daniel@budick.eu.
 
+
 #Activate scrapping:
-Comment out `throw new Meteor.Error(500, '...');` in /lib/methods.js Line 45.
+Comment out `throw new Meteor.Error(500, '...');` in /lib/methods.js Line 4 and 63. 
 
 # How to use it
 This application is made for local use.
@@ -20,7 +21,28 @@ You can scrap data yourself or you can use the dump with a lot of reports and ci
 `mongorestore --db meteor -h localhost --port 3001 --drop dump/meteor`
 
 If you want to scrap data yourself, you will have to fill the collection cities with data from /dump/metoer/cities.bson
-Then you have to comment out `throw new Meteor.Error(500, '...');` in /lib/methods.js Line 45. 
+Then you have to comment out `throw new Meteor.Error(500, '...');` in /lib/methods.js Line 4 and 63. 
+
+#Features
+Location detail:
+Every report is matched with a list of all zips in bavaria. Thus every report has a zip now.
+
+Category: Every report is categorised.
+Name Entity Recognition:
+Every report is fuzzysearched for each category. The categroy wiht the highest score is its category, if the score is over 1000 in the title or 10000 in the text.
+
+Synonym Name Entity Recognition:
+Every report that is not categorised is tested for synonyms of each category. If the score is high enough, the report is categorised, if the score is low, it is marked as unsafe categorised.
+
+Bag of words (work in progress):
+A bag of word is created for every report. The safely categorised report's bag of words is used to categorise the unsafe categorised reports.
+
+
+Home: A list of the first 500 reports to check if the categorisation works
+Map: A map of Bayern with some statistics (work in progress)
+Statistics: Some statistics compiled from the secure data (high score on categorisation)
+
+
 
 # Import DB to production server:
 `mongorestore -u <username> -p <password> -h <host>:<port> -d <database> dump/meteor`
--- client/index.js
+++ client/index.js
@@ -24,13 +24,18 @@ Template.index.helpers({
     },
     categorisedPercent: function () {
         if (Reports.find().count() !== 0) {
-            return Math.floor(Reports.find({processed:  'Categorised'}).count() / Reports.find().count()*1000)/10;}
+            return Math.floor(Reports.find({processed: 'Categorised'}).count() / Reports.find().count() * 1000) / 10;
+        }
         return 0;
     },
     unsafeCategorisedPercent: function () {
         if (Reports.find().count() !== 0) {
-            return Math.floor(Reports.find({processed:  {$ne: 'Not categorised'}}).count() / Reports.find().count()*1000)/10;}
+            return Math.floor(Reports.find({processed: {$ne: 'Not categorised'}}).count() / Reports.find().count() * 1000) / 10;
+        }
         return 0;
+    },
+    password: function () {
+        return Session.get('password');
     }
 });
 
@@ -48,6 +53,7 @@ function searchPage(start) {
     Meteor.call('getText', `https://www.polizei.bayern.de/bepo/news/presse/archiv/index.html?type=archiv&rubid=rub-4&query=&submit=Suchen%21&periodselect=All&period=fromto&periodfrom=01.01.2010&periodto=${moment().format('DD.MM.YYYY')}&start=${start}`, function (err, finished) {
         if (err) {
             console.error(err.error, err.message);
+            return;
         }
         if (Reports.find().count() < config.datasets || (config.datasets === -1 && !finished)) {
             return searchPage(start + 10);
@@ -64,7 +70,7 @@ Template.index.events({
     'click #name-entity-recognition': function () {
         Meteor.call('saveNameEntityRecognition');
     },
-    'click #w2v-name-entity-recognition': function(){
+    'click #w2v-name-entity-recognition': function () {
         Meteor.call('synonymNameEntityRecognition');
     },
     'click #bagOfWords': function () {
@@ -72,8 +78,16 @@ Template.index.events({
     },
     'click #load-data': function () {
         Meteor.call('empty', searchPage);
+    },
+    'click #reset-data': function () {
+        Session.set('password', true)
+    },
+    'click #reset-data-confirm': function () {
+        var code = document.getElementById('password').value;
+        Meteor.call('reset', code);
+        Session.set('password', false);
     }
 });
-
+Session.setDefault('password', false);
 
 
